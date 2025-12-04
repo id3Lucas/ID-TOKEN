d7b540d136ebdb1e863753e083d98462f82eded8
llhuillier <lucas.lhuillier@id3.eu>
2025-12-04 16:44:50 +0100

    feat: add initial setup for GitHub OAuth authentication with redirect URI handling and constants.

--- github_html_viewer/lib/constants.dart
+++ github_html_viewer/lib/constants.dart
@@ -2,10 +2,10 @@
 const String githubClientId = 'Ov23liFDMPNu223N42MW';
 
 // TODO: Replace with your actual GitHub OAuth Client Secret
-const String githubClientSecret = 'b35fd10539a883288777df823b48cb6f27dd0780';
+const String githubClientSecret = '60cb95f50239dddf86d9f8b60d228feffda0760e';
 
 // This must match the redirect URI configured in your GitHub OAuth App settings
-const String githubRedirectUri = 'https://github_html_viewer://callback';
+const String githubRedirectUri = 'http://localhost:8080/';
 
 const String githubAuthUrl =
     'https://github.com/login/oauth/authorize?client_id=$githubClientId&scope=repo,user&redirect_uri=$githubRedirectUri';
