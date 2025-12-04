7d4c7e8ef093fce8454ae0a9a4878a6e3cceca3c
llhuillier <lucas.lhuillier@id3.eu>
2025-12-04 16:23:31 +0100

    feat: add GitHub OAuth constants for client ID, secret, redirect URI, and authentication URLs.

--- github_html_viewer/lib/constants.dart
+++ github_html_viewer/lib/constants.dart
@@ -1,11 +1,11 @@
 // TODO: Replace with your actual GitHub OAuth Client ID
-const String githubClientId = 'YOUR_GITHUB_CLIENT_ID';
+const String githubClientId = 'Ov23liFDMPNu223N42MW';
 
 // TODO: Replace with your actual GitHub OAuth Client Secret
-const String githubClientSecret = 'YOUR_GITHUB_CLIENT_SECRET';
+const String githubClientSecret = 'b35fd10539a883288777df823b48cb6f27dd0780';
 
 // This must match the redirect URI configured in your GitHub OAuth App settings
-const String githubRedirectUri = 'github_html_viewer://callback';
+const String githubRedirectUri = 'https://github_html_viewer://callback';
 
 const String githubAuthUrl =
     'https://github.com/login/oauth/authorize?client_id=$githubClientId&scope=repo,user&redirect_uri=$githubRedirectUri';
